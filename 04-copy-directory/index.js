487b9a1039454465fd93e92cee87b909c213b14c
lenur77 <lenur77@gmail.com>
2023-05-09 17:25:41 +0300

    fix- add recursiveDelete folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,36 +1,54 @@
-/**Импорт всех требуемых модулей */
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const sourceDir = path.resolve(__dirname, 'files');
 const targetDir = path.resolve(__dirname, 'files-copy');
-/**Создание папки files-copy в случае если она ещё не существует */
-function copyDir(source, target) {
-  fs.mkdir(target, { recursive: true }, (err) => {
-    if (err) throw err;
-    /**Чтение содержимого папки files */
-    fs.readdir(source, { withFileTypes: true }, (err, files) => {
-      if (err) throw err;
-      files.forEach(file => {
-        const sourcePath = path.resolve(source, file.name);
-        const targetPath = path.resolve(target, file.name);
-       
-        /** Получение информации о файле (это папка или файл)*/
-        fs.stat(sourcePath, (err, stats) => {
-          if (err) throw err;
-          if (stats.isDirectory()) {
-            /**Создание папки в папке files-copy, если она есть */
-            copyDir(sourcePath, targetPath);
-          } else {
-            /**Копирование файлов из папки files в папку files-copy */
-            fs.copyFile(sourcePath, targetPath, err => {
-              if (err) throw err;
-            });
-          }
-        });
-      });
-    });
-  });
+
+async function deleteFolder(dirPath) {
+  try {
+    const files = await fs.readdir(dirPath, { withFileTypes: true });
+    for (const file of files) {
+      const currentPath = path.resolve(dirPath, file.name);
+      if (file.isDirectory()) {
+        await deleteFolder(currentPath);
+      } else {
+        await fs.unlink(currentPath);
+      }
+    }
+    await fs.rmdir(dirPath);
+  } catch (error) {
+      throw error;
+  }
 }
 
-copyDir(sourceDir, targetDir);
+async function copyDir(source, target) {
+  fs.mkdir(target, { recursive: true });
+
+  /**Чтение содержимого папки files */
+  try {
+    const files = await fs.readdir(source, { withFileTypes: true });
+    for (const file of files) {
+      const sourcePath = path.resolve(source, file.name);
+      const targetPath = path.resolve(target, file.name);
+      /** Получение информации о файле (это папка или файл)*/
+      if (file.isDirectory()) {
+        /**Создание папки в папке files-copy, если она есть */
+        copyDir(sourcePath, targetPath);
+      } else {
+        /**Копирование файлов из папки files в папку files-copy */
+        await fs.copyFile(sourcePath, targetPath);
+      }
+    }
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function main() {
+  await deleteFolder(targetDir);
+  await copyDir(sourceDir, targetDir);
+}
+
+main().catch(error => {
+  console.error('Error:', error);
+});
